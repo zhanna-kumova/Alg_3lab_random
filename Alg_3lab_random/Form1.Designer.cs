b8a4c2dfe89606027a31f905f5fdb2119d59696e
frezirovschik <frezirovschik@DESKTOP-FPFUT5P>
2020-05-17 03:12:01 +0300

    Add project files.

---
+++ Alg_3lab_random/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Alg_3lab_random
+{
+    partial class lab3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.first_btn = new System.Windows.Forms.Button();
+            this.sec_btn = new System.Windows.Forms.Button();
+            this.first_matrix = new System.Windows.Forms.ListBox();
+            this.second_matrix = new System.Windows.Forms.ListBox();
+            this.column = new System.Windows.Forms.Label();
+            this.col_exists = new System.Windows.Forms.Label();
+            this.max_label = new System.Windows.Forms.Label();
+            this.min_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // first_btn
+            // 
+            this.first_btn.Location = new System.Drawing.Point(12, 246);
+            this.first_btn.Name = "first_btn";
+            this.first_btn.Size = new System.Drawing.Size(170, 46);
+            this.first_btn.TabIndex = 1;
+            this.first_btn.Text = "column with even numbers";
+            this.first_btn.UseVisualStyleBackColor = true;
+            this.first_btn.Click += new System.EventHandler(this.first_btn_Click);
+            // 
+            // sec_btn
+            // 
+            this.sec_btn.Location = new System.Drawing.Point(364, 246);
+            this.sec_btn.Name = "sec_btn";
+            this.sec_btn.Size = new System.Drawing.Size(170, 25);
+            this.sec_btn.TabIndex = 2;
+            this.sec_btn.Text = "get the second matrix";
+            this.sec_btn.UseVisualStyleBackColor = true;
+            this.sec_btn.Click += new System.EventHandler(this.sec_btn_Click);
+            // 
+            // first_matrix
+            // 
+            this.first_matrix.FormattingEnabled = true;
+            this.first_matrix.ItemHeight = 16;
+            this.first_matrix.Location = new System.Drawing.Point(40, 50);
+            this.first_matrix.Name = "first_matrix";
+            this.first_matrix.Size = new System.Drawing.Size(98, 84);
+            this.first_matrix.TabIndex = 4;
+            // 
+            // second_matrix
+            // 
+            this.second_matrix.FormattingEnabled = true;
+            this.second_matrix.ItemHeight = 16;
+            this.second_matrix.Location = new System.Drawing.Point(396, 50);
+            this.second_matrix.Name = "second_matrix";
+            this.second_matrix.Size = new System.Drawing.Size(95, 84);
+            this.second_matrix.TabIndex = 5;
+            // 
+            // column
+            // 
+            this.column.AutoSize = true;
+            this.column.Location = new System.Drawing.Point(204, 100);
+            this.column.Name = "column";
+            this.column.Size = new System.Drawing.Size(105, 17);
+            this.column.TabIndex = 6;
+            this.column.Text = "column number";
+            // 
+            // col_exists
+            // 
+            this.col_exists.AutoSize = true;
+            this.col_exists.Location = new System.Drawing.Point(204, 60);
+            this.col_exists.Name = "col_exists";
+            this.col_exists.Size = new System.Drawing.Size(166, 17);
+            this.col_exists.TabIndex = 7;
+            this.col_exists.Text = "tag of column\'s existence";
+            this.col_exists.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // max_label
+            // 
+            this.max_label.AutoSize = true;
+            this.max_label.Location = new System.Drawing.Point(562, 60);
+            this.max_label.Name = "max_label";
+            this.max_label.Size = new System.Drawing.Size(33, 17);
+            this.max_label.TabIndex = 8;
+            this.max_label.Text = "max";
+            // 
+            // min_label
+            // 
+            this.min_label.AutoSize = true;
+            this.min_label.Location = new System.Drawing.Point(562, 100);
+            this.min_label.Name = "min_label";
+            this.min_label.Size = new System.Drawing.Size(30, 17);
+            this.min_label.TabIndex = 9;
+            this.min_label.Text = "min";
+            // 
+            // lab3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 373);
+            this.Controls.Add(this.min_label);
+            this.Controls.Add(this.max_label);
+            this.Controls.Add(this.col_exists);
+            this.Controls.Add(this.column);
+            this.Controls.Add(this.second_matrix);
+            this.Controls.Add(this.first_matrix);
+            this.Controls.Add(this.sec_btn);
+            this.Controls.Add(this.first_btn);
+            this.Name = "lab3";
+            this.Text = "317 Kumova, lab 3, var 13";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button first_btn;
+        private System.Windows.Forms.Button sec_btn;
+        private System.Windows.Forms.ListBox first_matrix;
+        private System.Windows.Forms.ListBox second_matrix;
+        private System.Windows.Forms.Label column;
+        private System.Windows.Forms.Label col_exists;
+        private System.Windows.Forms.Label max_label;
+        private System.Windows.Forms.Label min_label;
+    }
+}
+
---
+++ Alg_3lab_random/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alg_3lab_random
+{
+    public partial class lab3 : Form
+    {
+        public lab3()
+        {
+            InitializeComponent();
+        }
+
+        private static int[,] Random_matrix(ListBox listBox)
+        {
+            int[,] matrix = new int[3, 3];
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = rnd.Next(50);
+                }
+            }
+            string zer = Convert.ToString(matrix[0, 0]);
+            string fir = Convert.ToString(matrix[0, 1]);
+            string sec = Convert.ToString(matrix[0, 2]);
+            string[] values = new string[] { zer, fir, sec };
+            String row = String.Join(" ", values);
+
+            string zer1 = Convert.ToString(matrix[1, 0]);
+            string fir1 = Convert.ToString(matrix[1, 1]);
+            string sec1 = Convert.ToString(matrix[1, 2]);
+            string[] values1 = new string[] { zer1, fir1, sec1 };
+            String row1 = String.Join(" ", values1);
+
+            string zer2 = Convert.ToString(matrix[2, 0]);
+            string fir2 = Convert.ToString(matrix[2, 1]);
+            string sec2 = Convert.ToString(matrix[2, 2]);
+            string[] values2 = new string[] { zer2, fir2, sec2 };
+            String row2 = String.Join(" ", values2);
+
+            listBox.Items.Insert(0, row);
+            listBox.Items.Insert(1, row1);
+            listBox.Items.Insert(2, row2);
+
+            return matrix;
+        }
+
+        private void first_btn_Click(object sender, EventArgs e)
+        {
+            if (first_matrix.Items.Count == 0)
+            {
+                int[,] matrix = Random_matrix(first_matrix);
+                //int[,] matrix = { {2,9,13 }, {4,3,5 }, { 18, 0, 7} }; for debugging
+                bool brk = false;
+                int flag = 0;
+                int col = 0;
+                int num = 0; //for debugging
+                for (int i = 0; i < 3; i++)
+                {
+                    if (brk == true) { break; }
+                    if (flag < 3) { flag = 0; }
+                    for (int j = 0; j < 3; j++)
+                    {
+                        num = matrix[j, i]; //for debugging
+                        if ((matrix[j, i] % 2) == 0)
+                        {
+                            if (flag < 3)
+                            {
+                                flag += 1;
+                            }
+                            if (flag == 3)
+                            {
+                                col = i + 1;
+                                brk = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            flag = 0;
+                        }                    
+                    }
+                }
+                if (flag == 3)
+                {
+                    col_exists.Visible = true;
+                    column.Visible = true;
+                    col_exists.Text = "column exists";
+                    column.Text = col.ToString() + " " + "column";
+                }
+                else
+                {
+                    col_exists.Text = "column doesn't exist";
+                    column.Text = "";
+                }
+            }
+            else {
+                first_matrix.Items.Clear();
+                column.Visible = false;
+                col_exists.Text = "list has been cleared,\n press again ";
+            }
+        }
+
+        private void sec_btn_Click(object sender, EventArgs e)
+        {
+            int min = int.MaxValue, max = int.MinValue;
+
+            if (second_matrix.Items.Count == 0)
+            {
+                int[,] matrix2 = Random_matrix(second_matrix);
+                for (int row = 0; row < 3; row++)
+                    for (int col = 3 - row; col < 3; col++)
+                        if (matrix2[row, col] > max)
+                            max = matrix2[row, col];
+                max_label.Text = max.ToString() + " - " + "maximum number";
+
+                for (int row = 0; row < 3; row++)
+                    for (int col = row + 1; col < 3; col++)
+                        if (matrix2[row, col] < min) 
+                            min = matrix2[row, col];
+                min_label.Visible = true;
+                min_label.Text = min.ToString() + " - " + "minimum nuber";
+            }
+            else {
+                second_matrix.Items.Clear();
+                min_label.Visible = false;
+                max_label.Text = "list has been cleared,\n press again ";
+            }      
+        }
+    }
+}
